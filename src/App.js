10fcae5efa7d33662a4a6d8288ef1d3b646cc12e
nivya-dileep <nivyadileep2000.eee@gmail.com>
2023-08-14 23:38:30 +0530

    image change

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={"https://hips.hearstapps.com/hmg-prod/images/close-up-of-blossoming-rose-flower-royalty-free-image-1580853844.jpg"} style={{width:500, height:500}} alt="logo" />
         <p>
           Edit nivya
         </p>
